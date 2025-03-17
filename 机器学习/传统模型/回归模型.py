b3c2cb78373824f847b6c193afbf2523d5d1fd0a
bailiao1 <193362772@qq.com>
2025-03-17 19:25:36 -0400

    Update 回归模型.py

--- 机器学习/传统模型/回归模型.py
+++ 机器学习/传统模型/回归模型.py
@@ -1,30 +1,5 @@
 import numpy as np
 
-class LinearRegressionScratch:                            # 定义类
-    def __init__(self):                                   # init初始化类
-        self.coef_ = None                                 # 设self.coef(系数)
-        self.intercept_ = None                            # 设self.intercept_(截距)
-
-    def fit(self, X, y):                                          # 定义方法(传入训练数据,结果)
-        X_b = np.c_[np.ones((X.shape[0], 1)), X]                  # 增加偏置项
-        theta = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)   # 正规方程求解
-        self.intercept_ = theta[0]                                # 偏置项
-        self.coef_ = theta[1:]                                    # 真正的模型权重参数
-
-    def predict(self, X):                                         # 定义方法(传入数据，预测结果)
-        X_b = np.c_[np.ones((X.shape[0], 1)), X]                  # 也要增加偏置项
-        return X_b @ np.r_[self.intercept_, self.coef_]           # 组合参数(theta)
-
-# 测试
-X = np.array([[1], [2], [3], [4]])
-y = np.array([2, 3, 4, 5 ])                                       # y = x+1
-
-model = LinearRegressionScratch()
-model.fit(X, y)
-print(model.predict(np.array([[5]])))  # 预测 5 的值        # [6.]
-
-
-
 # 岭回归   Ridge(alpha=1.0)  # λ = 1.0(alpha控制L2正则强度,使一些特征权重缩小，限制模型的复杂度)
 #         theta = np.linalg.inv(X_b.T.dot(X_b)+λ*I).dot(X_b.T).dot(y)    (L2正则公式,I=单位矩阵）
 
