689f4ecdab23f46179bf292b82cdec283647467f
bailiao1 <193362772@qq.com>
2025-04-21 19:25:34 -0400

    Update 数据容器.py

--- Python/初阶/数据容器.py
+++ Python/初阶/数据容器.py
@@ -11,17 +11,23 @@ print(a[3][0])                                          # 4 (取出第 4 个值�
 print(a[0:3:2])                                         # [1, '3'] (取出第1到第4的值，步长为2)
 
 
-
 # 添加元素
-
-# 1,整体添加
-b.append("hello")
+b.append("hello")                                       # 整体添加
 print(b)                                                # ['hello']
 
-# 2,分散添加(仅限可迭代对象:字符串,列表......)
-b.extend("ld")
+b.extend("ld")                                          # 分散添加(仅限可迭代对象:字符串,列表......)
 print(b)                                                # ['hello', 'l', 'd'] ("ld"字符串被拆分成一个个单独的字符)
 
-# 3,指定位置添加
-b.insert(1,'wor')
+b.insert(1,'wor')                        # 指定位置添加
 print(b)                                                # ['hello', 'wor', 'l', 'd'] 在原本索引[1]的插入
+
+
+# 移除元素
+b.remove('wor')                                         # 根据元素移除
+print(b)                                                # ['hello', 'l', 'd']
+
+del b[2]                                                # 根据下标移除
+print(b)                                                # ['hello', 'l']
+
+c = b.pop(1)                                            # 移除指定小标并返回值                           
+print(f"b:{b} c:{c}")                                   # b:['hello'] c:l
