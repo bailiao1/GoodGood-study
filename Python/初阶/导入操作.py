9e838c9bfa5bde2ea3246bd97fee4cce9b67d5d7
bailiao1 <193362772@qq.com>
2025-05-09 09:46:20 -0400

    Create 导入操作.py

---
+++ Python/初阶/导入操作.py
@@ -0,0 +1,8 @@
+# import xxx as xx
+# 导入xxx模块，并取别名为xx，调用时不需要写完整xxx.xx()方法，只需要xx.xx()
+
+# from xx import xx as xx
+# 从xx模块单独导入xx类、函数、变量，并取别名为xx(有时候不是所有东西都用得上)
+
+# form xx import *
+# 从某模块导入所有类、函数、变量 (不推荐使用，待会命名空间污染，也看不出某函数到底来自哪个库)
