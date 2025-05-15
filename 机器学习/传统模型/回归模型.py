6e4a1a7547507402584c75292302e01d37b05330
bailiao1 <193362772@qq.com>
2025-05-15 10:31:35 -0400

    Update 回归模型.py

--- 机器学习/传统模型/回归模型.py
+++ 机器学习/传统模型/回归模型.py
@@ -57,5 +57,42 @@ print(lasso.predict(np.array([[5]])))
 print("Lasso 计算的 theta:", lasso.theta)      
 
 
-# 逻辑回归 LogisticRegression(max_iter=1000,solver="lbfgs")  # max_iter(最大迭代次数),solver="lbfgs"(最常用的优化方法)
-# 逻辑回归是分类算法,适用于二分类问题
+# LogisticRegression(max_iter=1000,solver="lbfgs")  # sklean写法： max_iter(最大迭代次数),solver="lbfgs"(最常用的优化方法)
+# 逻辑回归是分类算法,适用于二分类问题(预测一件事成立的概率，大于50% 或 小于50% ，它是猫的概率有80% 不是的概率有 20%，所以推测出 1，是猫。0 则是其他动物)
+# numpy手写实现：
+class Logistic Regression():
+    def __init__(self,lr=0.1,epochs=1000):                 # 输入学习率lr 以及 训练轮数
+        self.lr = lr                                        
+        self.epochs = epochs
+        self.theta = None
+        
+    def sigmoid(self,x):                                   # sigmoid激活函数，将输出映射到[0,1]适用于二分类
+        return 1 / (1 + np.exp(-x))                        # 1 / (1 + e^-x)
+
+    def compute_loss(self,y,y_hat):                        # 计算交叉熵损失
+        ep = 1e-15                                         # 设置个最小值，防止下面计算log时，出现0
+        y_hat = np.clip(y_hat,ep ,1-ep)                                           # 限制数值大小
+        return -np.mean(y * np.log(y_hat) + (1-y) * np.log(1 - y_hat))            
+
+    def fit(self,x,y):
+        m,n = x_b.shape[-2:]                               # 便利获取数据的样本数，特征数
+        x_b = np.c_[np.ones((m,1)),x]                      # 添加偏置b
+        self.theta = np.zeros(n)                           # 初始化w向量
+
+        for epoch in range(epochs):                        # 设置训练循环，每轮预测一个值，进行一次梯度下降，不断向最优(极值点)靠近，步长由学习率控制，太大错过最优，太小走得太慢
+            z = x @ theta                                  # 预测
+            y_hat = self.sigmoid(z)                        
+
+            gradient = x_b @ (y_hat - y) / m               # 计算梯度，loss的导数，找到前往极值点的方向
+            self.theta -= self.lr * gradient               # 梯度下降 
+
+            if epoch % 100 == 0:                           # 每100轮打印一次loss，方便监控
+                loss = self.compute_loss(y,y_hat)
+                print(f"Epoch: {epoch} , Loss: {loss:.4f}")
+
+    def perdict(self,x):                                   # 训练完成后就可以预测了
+        x_b = np.c_[]np.ones((x.shape[-2],1)),x]           # 同样给需要预测的样本添加偏置项
+        out = self.sigmoid(x_b @ self.theta)               # 计算 xw+b
+        return (out >= 0.5).astype('int')                  # 再将输出映射到[1,0]区间，转换成概率，概率大于等于0.5就输出1，否则为0
+            
+
