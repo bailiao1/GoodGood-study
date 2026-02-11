0e6475736baec7ef2164cc9ca1b5b7ef1fc2b1f4
bailiao1 <193362772@qq.com>
2026-02-10 20:30:00 -0500

    Update 文件操作.bash

--- Linux/文件操作.bash
+++ Linux/文件操作.bash
@@ -48,8 +48,8 @@ rm -r new_floder/*   # 删除所有文件和子目录
 
 # 查看文件
 cat file.txt           # 显示整个文件
-less file.txt          # 分页查看（可向前也能向后, 1 <-> 2）
-more file.txt          # 分页查看（只能向前查看, 1 -> 2）
+less file.txt          # 分页查看（可向前也能向后 1 <-> 2）
+more file.txt          # 分页查看（只能前向查看 1 -> 2）
 
 hand -n 10 file.txt    # 查看开头10行
 tail -n 10 file.txt    # 查看尾部10行
@@ -60,7 +60,6 @@ tail -s 1 -f log.txt   #  则表示每秒检查一次（-s 指定间隔秒数）
 
 tail -F log.txt        # 大写F，跟踪文件名
                        # 当创建新的log.txt时，f会继续保持跟踪原文件，而F会重新打开新文件，转而跟踪新文件log.txt的内容
-
 
 # 搜索文本
 grep "xxx" file.txt    # 在file文件中，搜索包含"xxx"的行
@@ -76,9 +75,16 @@ sort -r                # 反向排序（倒序）
 sort -u                # 先排序+按排序键去重（可能会丢掉“不同内容但 key 相同”的行）
 sort file.txt | uniq   # uniq 去重相邻且相同的行
 
-
 sort -k2               # 用第2个字段作为排序键（默认字段分隔符是 空格/tab）
 sort -k2,2             # 只看第二列字段（从2开始，到2结束）
 sort -t,               # 用"，"作为分隔符
+
+# 计数
+wc file.txt            # 统计行数，单词数，字节数
+wc -l                  # 统计行数
+wc -w                  # 统计单词数
+wc -c                  # 统计字节数
+wc -m                  # 统计字符数
+
 
                   
